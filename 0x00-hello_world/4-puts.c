bba301a986ebe05db24c5bf880820780a7b4a24a
philohere <2497@holbertonschool.com>
2020-09-17 04:34:36 +0000

    4. Using the puts command instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
-/*
+/**
  * main- void
  *Description: Display text on screen
  *Return: 0
-*/
+ **/
 
 int main(void)
 {
 
-  puts("\"Programming is like building a multilingual puzzle \n");
+  puts("\"Programming is like building a multilingual puzzle");
 
  return (0);
 }

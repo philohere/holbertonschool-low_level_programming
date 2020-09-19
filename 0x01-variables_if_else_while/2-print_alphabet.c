c8d5fe5954b9312e8cf9114775c4410f0c148c48
philohere <2497@holbertonschool.com>
2020-09-19 03:08:19 +0000

    2. Print the alphabet on one line with a new line after

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main- void
+*
+* Description: Displays the alphabet in lowercase
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	{
+	putchar('\n');
+	}
+	return (0);
+}

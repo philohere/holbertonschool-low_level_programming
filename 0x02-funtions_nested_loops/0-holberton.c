ced3f0d5dcdb842ed4a943b8d4b9a912d8d81ac7
philohere <2497@holbertonschool.com>
2020-09-22 05:00:38 +0000

    0. Using a local funtion as a header to run code

---
+++ 0x02-funtions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "_putchar.h"
+
+/**
+*main - void
+*
+*Description: Display Holberton by calling on another function
+*
+*Return: 0
+*/
+
+int main(void)
+
+{
+	char c[] = "Holberton";
+	int i;
+
+for (i = 0; i <= 9; i++)
+_putchar(c[i]);
+
+_putchar('\n');
+
+return (0);
+}

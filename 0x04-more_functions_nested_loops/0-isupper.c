2d9ba7f01a1342edc3caae187d2ae45b2e3be959
philohere <2497@holbertonschool.com>
2020-09-24 20:27:59 +0000

    0. Check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check if a letter is upper or lower case.
+ *
+ * @c:
+ *
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

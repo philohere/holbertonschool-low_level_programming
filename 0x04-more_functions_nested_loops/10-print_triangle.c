90c5d0a58eebd4f0abe937bc9257d5c94ea529a7
philohere <2497@holbertonschool.com>
2020-09-25 02:18:02 +0000

    10. Print Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - Print a triangle
+ * @size: variable input
+ * Return: void
+ */
+
+print_triangle(int size)
+{
+	return (0);
+}

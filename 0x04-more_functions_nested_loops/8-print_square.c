b0528a006275f2bac8c0a31e1d4ba5b405f530db
philohere <2497@holbertonschool.com>
2020-09-25 02:03:23 +0000

    8. Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a square
+ * @size: pull size
+ * void
+ */
+
+void print_square(int size)
+{
+
+	int row;
+	int col;
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}

37565319830887fe294425bf23649f31146ff42d
philohere <2497@holbertonschool.com>
2020-09-25 23:01:47 +0000

    9. FizzBuzz chalenge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d"\n, i);
+			printf("%d ", i);
 
 		else if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz\n");
+			printf("Fizz ");
 
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz\n");
+			printf("Buzz ");
 
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz\n");
+			printf("FizzBuzz ");
 	}
 	printf("\n");
 	return (0);

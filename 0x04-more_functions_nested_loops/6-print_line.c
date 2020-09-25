c40bedd067b1917968b47a2a289a8e949f005c31
philohere <2497@holbertonschool.com>
2020-09-25 00:33:02 +0000

    6. Print the number of _ based on the variable

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_line - Draw a straight line
+ *
+ *@n: this is the number of times to print _ character
+ *
+ *Return: void
+ */
+
+void print_line(int n)
+{
+
+	if (n > 0)
+	{
+		while (--n)
+			_putchar('_');
+		_putchar('\n');
+				}
+	else
+		_putchar('\n');
+}

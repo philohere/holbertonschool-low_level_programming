19337e322d8b1fce1c3dc2593b2cb05dd49af76a
philohere <2497@holbertonschool.com>
2020-09-29 02:18:45 +0000

    1. Swap the value of two integers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Takes a pointer to an integer twice
+ * @a: This is a pointer variable
+ * @b: This is a pointer variable
+ * Return - void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+		*b = 98;
+}

5cf64817225520c58584b70974e5c32d04bb6801
philohere <2497@holbertonschool.com>
2020-09-30 04:21:42 +0000

    7. Print the first half of number if even and second half if odd.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * _strlen - counts the number of characters in a string
+ * @s: This is a pointer to a string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+        int count = 0;
+
+        while (s[count] != '\0')
+        {
+                count++;
+        }
+	return (count);
+}
+
+/**
+ * puts_half - prints half of the string
+ * @str: string of characters
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count = _strlen(str);
+	int a;
+	int b;
+	int c;
+
+	if (count % 2 != 0)
+	{
+		c = count / 2;
+		for (a = 0; a < c; a++)
+			_putchar(str[a]);
+	 }
+		if (count % 2 == 0)
+		{
+			c = count / 2;
+			for (b = c; str[b] != '\0'; b++)
+		_putchar(str[b]);
+	    }
+	_putchar('\n');
+}

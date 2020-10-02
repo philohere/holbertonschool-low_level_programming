9df5ee261d0f533d11209aac20ad3890e2b7502d
philohere <2497@holbertonschool.com>
2020-10-02 00:19:19 +0000

    3. Compares two strings and prints the difference

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (s1[i] != '\0' && s2[j] != '\0')
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 	i++;
-	j++;
-}
-	return (*s1 - *s2);
+	}
+	if (s1[i] == s2[i])
+	return (0);
+	else
+		return (s1[i] - s2[i]);
 }
